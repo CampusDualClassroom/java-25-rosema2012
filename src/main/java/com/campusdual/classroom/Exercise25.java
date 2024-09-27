5b6afbd56f84f77e04cc7b80fbc6646bbcfcfdb9
Happy Days!!! <36199346+rosema2012@users.noreply.github.com>
2024-09-27 13:51:32 +0200

    Ejercicio 25 terminado

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -3,17 +3,32 @@ package com.campusdual.classroom;
 import java.util.Stack;
 
 public class Exercise25 {
-    public static Stack<String> createStack() {
 
+    public static Stack<String> createStack() {
+        Stack <String> pila = new Stack<>();
+        pila.push("Smith");
+        pila.push("Montessori");
+        pila.push("Peralta");
+        pila.push("House");
+        return pila;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
+        while(!stack.isEmpty())
+        {
 
+            System.out.println(stack.peek());
+            stack.pop();
+        }
     }
 
     public static void main(String[] args) {
-
+        Stack <String> pila = createStack();
+        printAndEmptyStack(pila);
     }
+
+
+
 
 
 
